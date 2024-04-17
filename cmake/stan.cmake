cbb9e4a7e265fb40eb45d87363cd7a09e4462d24
Alec Jacobson <alecjacobson@adobe.com>
2024-04-17 14:13:40 -0400

    seems like a hack

--- cmake/stan.cmake
+++ cmake/stan.cmake
@@ -22,6 +22,10 @@ add_dependencies(${PROJECT_NAME} stan)
 add_library(stan::stan INTERFACE IMPORTED GLOBAL)
 set(stan_BOOST_INCLUDE_DIR ${stan_INSTALL}/lib/boost_1.81.0)
 set(stan_SUNDIALS_INCLUDE_DIR ${stan_INSTALL}/lib/sundials_6.1.1/include/)
+# make temporary folder at stan_BOOST_INCLUDE_DIR and stan_SUNDIALS_INCLUDE_DIR
+# so that it exists now. It will be downloaded there.
+file(MAKE_DIRECTORY ${stan_BOOST_INCLUDE_DIR})
+file(MAKE_DIRECTORY ${stan_SUNDIALS_INCLUDE_DIR})
 target_include_directories(stan::stan INTERFACE ${stan_INCLUDE_DIR} ${stan_BOOST_INCLUDE_DIR} ${stan_SUNDIALS_INCLUDE_DIR})
 # Why is this needed but not defined by default?
 target_compile_definitions(stan::stan INTERFACE "_REENTRANT")
